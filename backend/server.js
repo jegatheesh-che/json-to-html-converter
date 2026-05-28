9ebf525dfef1b5e2bc31667888bd6e9205c48a0e
Jegatheesh <jegatheeesh.s@gmail.com>
2026-05-28 19:52:16 +0530

    render deploy fix

--- backend/server.js
+++ backend/server.js
@@ -226,6 +226,8 @@ app.post("/upload", upload.single("file"), (req, res) => {
   res.download(outputPath);
 });
 
-app.listen(3000, () => {
-  console.log("🚀 Server started on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server started on port ${PORT}`);
 });
